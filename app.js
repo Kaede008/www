d8a52466c02338551e2b3e17ca23d2f230aa3985
kaede <kaede008@qq.com>
2019-06-23 12:41:10 +0800

    test pull function

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(async (ctx, next) => {
-    await next()
     ctx.response.type = "text/html"
-    ctx.response.body = `<h1>Hello Bwh!</h1>`
+    ctx.response.body = `<h1>你好, 欢迎来到四爷的个人网站!</h1>`
 })
 
 app.listen(3000, () => {
-    
+    console.log('server is running at http://localhost:3000')
 })
